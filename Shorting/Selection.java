2eea453e4971daeaf63924a9e908730c673b8bc8
BigilAnkit <gehlotankit19@gmail.com>
2024-09-15 01:32:06 +0530

    3 Shorting & String

---
+++ Shorting/Selection.java
@@ -0,0 +1,25 @@
+ class Selection1{
+
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,3,4,2,1,6};
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j] <arr[smallest]){
+                    smallest = j;
+                }
+            }
+            // swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        printArray(arr);
+    }
+}
